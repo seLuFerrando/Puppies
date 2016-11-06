dd856be481424a6f5370b6a3e2762bb5ff6561d8
seLuFerrando <seLuFerrando@users.noreply.github.com>
2016-11-06 02:21:03 +0100

    Part two

--- app/src/main/java/com/selumobileapps/puppies/FavoritesActivity.java
+++ app/src/main/java/com/selumobileapps/puppies/FavoritesActivity.java
@@ -1,32 +1,31 @@
 package com.selumobileapps.puppies;
 
-import android.content.Intent;
-import android.support.design.widget.FloatingActionButton;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+/**
+ * Created by selu on 6/11/16.
+ */
 
+public class FavoritesActivity extends AppCompatActivity {
     ArrayList<Puppy> puppies;
-    FloatingActionButton fab;
     private RecyclerView puppiesList;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_favorites);
 
         Toolbar actionBar = (Toolbar) findViewById(R.id.actionBar);
         setSupportActionBar(actionBar);
         getSupportActionBar().setIcon(R.drawable.footprint);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         puppiesList = (RecyclerView) findViewById(R.id.rvPuppies);
         LinearLayoutManager llm = new LinearLayoutManager(this);
@@ -34,30 +33,17 @@ public class MainActivity extends AppCompatActivity {
         puppiesList.setLayoutManager(llm);
         iniPuppiesList();
         iniPuppiesAdapter();
-
-        fab = (FloatingActionButton) findViewById(R.id.fabCamera);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //TODO fab click
-            }
-        });
-
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu, menu);
+        getMenuInflater().inflate(R.menu.menu_favorites, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-            case R.id.itemFavorites:
-                Intent intent = new Intent(this, FavoritesActivity.class);
-                startActivity(intent);
-                return true;
             case R.id.itemSettings:
                 //TODO item click
                 return true;
@@ -68,9 +54,6 @@ public class MainActivity extends AppCompatActivity {
 
     public void iniPuppiesList(){
         puppies = new ArrayList<Puppy>();
-        puppies.add(new Puppy(R.drawable.puppy, "Satcha", 5, false));
-        puppies.add(new Puppy(R.drawable.puppy2, "Rocko"));
-        puppies.add(new Puppy(R.drawable.puppy3, "Kuka", 3));
         puppies.add(new Puppy(R.drawable.puppy4, "Tronk"));
         puppies.add(new Puppy(R.drawable.puppy5, "Drako"));
         puppies.add(new Puppy(R.drawable.puppy6, "Mari"));
@@ -82,4 +65,6 @@ public class MainActivity extends AppCompatActivity {
         PuppyAdapter adapter = new PuppyAdapter(puppies);
         puppiesList.setAdapter(adapter);
     }
+
+
 }
